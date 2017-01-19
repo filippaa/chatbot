349495c579d936bb38b177e25e9691178fffc66a
filippaa <lippaallinen@hotmail.com>
2017-01-19 12:45:09 +0200

    skapade en startsida med formulär för namn

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+<form>
+	<label for="name">Name</label>
+	<input type="text" name="name" id="name">
+	<input type="submit" value="Send!">
+</form>
+</body>
