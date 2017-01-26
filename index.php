18621b2d77d17fde22e7eb8ab613614dd6ea9b07
filippaa <lippaallinen@hotmail.com>
2017-01-26 14:38:51 +0200

    ugh

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
 <form>
-	<label for="name">Name</label>
+	<label for="name">kill me</label>
 	<input type="text" name="name" id="name">
 	<input type="submit" value="Send!">
 </form>
